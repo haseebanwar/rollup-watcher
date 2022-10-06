92ee71a422bd60ff6c584d05dde48a0d224f4708
Haseeb Anwar <haseebanwar99@gmail.com>
2022-10-06 16:23:41 +0500

    Add helper text

--- playground/index.js
+++ playground/index.js
@@ -1,3 +1,5 @@
+// this tells rollup to look for "module" key in package.json
+// which then points to `dist/index.js` (output of the first bundle)
 import { text } from '../';
 
 // this text is coming from 'dist/index.js'
